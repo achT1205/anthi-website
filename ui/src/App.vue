bc34508e72b419f2efc6e5354fa92d320947984e
fakher90 <mr.fk.chouchane@gmail.com>
2021-01-14 18:04:33 +0100

    change

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,186 +1,30 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar app flat height="64" class="grey lighten-3">
-      <v-container class="py-0 fill-height pa-0">
-        <v-btn
-          elevation="0"
-          class="nav-tabs-item"
-          active-class="active-nav-tabs-item"
-          to="/home"
-          >HOME</v-btn
-        >
-        <v-btn
-          elevation="0"
-          class="nav-tabs-item"
-          active-class="active-nav-tabs-item"
-          to="/research"
-          >SEARCH</v-btn
-        >
-
-        <v-btn
-          elevation="0"
-          class="nav-tabs-item-other ml-10"
-          active-class="active-nav-tabs-item"
-          to="/work-with-me"
-          >WORK WITH ME</v-btn
-        >
-        <!--<v-spacer></v-spacer>
-        <v-responsive max-width="260">
-          <v-text-field
-            solo
-            label="Search"
-            append-icon="fas fa-search"
-            flat
-            hide-details
-            rounded
-            color="white"
-          ></v-text-field>
-        </v-responsive>-->
-      </v-container>
-    </v-app-bar>
-
-    <v-main class="grey lighten-3">
-      <v-container>
-        <v-row>
-          <div class="navigation-drawer pa-0 animate__animated"
-          :class="stickySidebar ? 'sticky animate__fadeInUp': 'col col-lg-3 col-sm-4'">
-            <v-sheet
-              rounded="lg"
-              color="#B7A99A"
-              class="profile-card"
-            >
-              <div class="d-flex justify-center">
-                <span class="firtName">Evanthia</span>
-                <span class="lastName ml-2">DIMARA</span>
-              </div>
-              <div class="d-flex justify-center mt-2">
-                <v-avatar size="150">
-                  <img
-                    src="@/assets/images/evanthia_dimara.png"
-                    alt="Evanthia Dimara"
-                  />
-                </v-avatar>
-              </div>
-              <div class="d-flex justify-center mt-2">
-                <span class="title">Assistant Professor</span>
-              </div>
-              <div class="d-flex justify-end she-her">
-                <v-spacer></v-spacer>
-                <span>She/Her</span>
-              </div>
-              <div class="d-flex justify-center utrechtUniversity">
-                <a
-                  href="http://www.cs.uu.nl/staff/dimara.html"
-                  target="_blank"
-                  class="ml-2"
-                >
-                  <img
-                    height="35"
-                    src="@/assets/images/UtrechtUniversity_logo_EN_WHITE.png"
-                    alt="Utrecht University"
-                  />
-                </a>
-              </div>
-              <div class="d-flex justify-center mt-2">
-                <h4>Contact me via</h4>
-              </div>
-
-              <div class="d-flex justify-center">
-                <v-icon x-small color="white">fas fa-envelope</v-icon>
-                <a
-                  href="mailto:evanthia.dimara@gmail.com"
-                  target="_blank"
-                  class="ml-2 email-href"
-                  >evanthia.dimara@gmail.com</a
-                >
-              </div>
-              <div class="d-flex justify-center mt-2">
-                <h4>Connect with me via</h4>
-              </div>
-              <div class="d-flex justify-center">
-                <a
-                  href="https://scholar.google.fr/citations?user=megudlUAAAAJ&hl=en"
-                  target="_blank"
-                  class="ml-2"
-                >
-                  <v-icon small color="white">fa fa-graduation-cap</v-icon>
-                </a>
-                <a
-                  href="https://twitter.com/anthi_dimara"
-                  target="_blank"
-                  class="ml-2"
-                >
-                  <v-icon small color="white">fa fa-twitter</v-icon>
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/evanthiadimara/"
-                  class="ml-2"
-                >
-                  <v-icon color="white">fa fa-linkedin</v-icon>
-                </a>
-                <a
-                  href="https://www.mendeley.com/profiles/evanthia-dimara2/"
-                  target="_blank"
-                  class="ml-2"
-                >
-                  <v-icon small color="white">fab fa-mendeley</v-icon>
-                </a>
-
-                <a
-                  href="https://www.researchgate.net/profile/Evanthia_Dimara"
-                  target="_blank"
-                  class="ml-2"
-                >
-                  <v-icon small color="white">fab fa-researchgate</v-icon>
-                </a>
-                <a
-                  href="https://orcid.org/0000-0001-5212-7888"
-                  target="_blank"
-                  class="ml-2"
-                >
-                  <v-icon small color="white">fab fa-orcid</v-icon>
-                </a>
-              </div>
-              <div class="d-flex justify-center mt-4">
-                <h4>My CV</h4>
-                <a href="" target="_blank" class="ml-2">
-                  <v-icon color="white">fa fa-file-pdf</v-icon>
-                </a>
-              </div>
-              <div class="d-flex justify-center my-2">
-                <h4>Mailing Address</h4>
-                <a
-                  href="https://goo.gl/maps/Gjphw3XGb4rX3VhU8"
-                  target="_blank"
-                  class="ml-2"
-                >
-                  <v-icon color="white">fas fa-search-location</v-icon>
-                </a>
-              </div>
-              <div class="d-flex justify-center">
-                <span class="mx-2 mb-2 department-text">
-                  Department of Information & Computing Sciences, Faculty of
-                  Science, Utrecht University Princetonplein 5 3584 CC Utrecht,
-                  Netherlands
-                </span>
-              </div>
-            </v-sheet>
+    <v-container class="py-0">
+      <v-row>
+        <Menu />
+      <div class="page-content">
+        <div class="content">
+          <Sidebar />
+          <div class="views">
+            <router-view></router-view>
           </div>
-          <div class="col " :class="stickySidebar ? 'col-12 is-sticky': 'pa-0 col-lg-9 col-sm-8'">
-            <div class="views" min-height="70vh" rounded="lg">
-              <router-view></router-view>
-            </div>
-          </div>
-        </v-row>
-      </v-container>
-    </v-main>
+        </div>
+      </div>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-
+import Menu from '@/components/menu.vue';
+import Sidebar from '@/components/sidebar.vue'
 export default {
+  components:{
+    Menu,
+    Sidebar,
+  },
   data: () => ({
     links: ["HOME", "RESEARCH", "WORK WITH ME"],
     stickySidebar: false,
@@ -203,16 +47,28 @@ export default {
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap");
-@import "~animate.css";
-
+body {
+  font-size: $fontSize;
+  font-family: $fontFamily;
+  color: $darkenBrown;
+}
+.page-content{
+  width: 100%;
+  padding-top: 45px;
+  .content{
+    display: flex;
+    width: 100%;
+  }
+  
+}
+.v-main{
+  padding-top: 60px!important;
+}
 .lastName {
   font-weight: bold;
   font-size: 32px;
 }
-h1,h2,h3,h4,h5,h6{
-  font-weight: 900;
-}
+
 .firtName {
   font-weight: 200;
   font-size: 32px;
@@ -222,28 +78,8 @@ h1,h2,h3,h4,h5,h6{
   margin-top: -10px;
   margin-right: 61px;
 }
-.profile-card {
-  background-color: #b7a99a;
-  color: white !important;
-}
 
-.navigation-drawer {
-  z-index: 99 !important;
-  border-style: solid !important;
-  border-width: 1.5px !important;
-  border-color: #504538 !important;
-  border-radius: 6px 6px;
-  background-color: #b7a99a;
-  color: white !important;
-  margin-top: 0px !important;
-  height: 555px;
-  transition: outline 500ms ease-in;
-}
-a:link {
-  color: white;
-  background-color: transparent;
-  text-decoration: none;
-}
+
 .utrechtUniversity {
   margin-top: -10px;
 }
@@ -323,13 +159,12 @@ body {
   font-size: 14px;
 }
 .views{
-  background-color: #FFFFFF;
-  border-color: #FFFFFF;
-  color: rgba(0, 0, 0, 0.87);
-  box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.2), 0px 0px 0px 0px rgba(0, 0, 0, 0.14), 0px 0px 0px 0px rgba(0, 0, 0, 0.12);
-  border-radius: 8px;
-  min-height: 70vh;
-  padding: 1rem;
+  //color: rgba(0, 0, 0, 0.87);
+  //box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.2), 0px 0px 0px 0px rgba(0, 0, 0, 0.14), 0px 0px 0px 0px rgba(0, 0, 0, 0.12);
+  border-radius: $radius;
+  //min-height: 70vh;
+  padding: 0 .5rem;
+  width: calc(100% - 300px);
 }
 @media(max-width: 992){
   .v-list-item{
---
+++ ui/src/components/menu.vue
@@ -0,0 +1,61 @@
+<template>
+  <header>
+    <v-tabs>
+        <v-tab
+        class="item-link"
+        :title="item.title" 
+        v-for="(item, index) in items"
+        :key="index"
+        :link="true"
+        :to="item.link">
+          {{item.title}}
+        </v-tab>
+    </v-tabs>
+  </header>
+</template>
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        title: 'Home',
+        link: '/home'
+      },
+      {
+        title: 'Research',
+        link: '/research'
+      },
+      {
+        title: 'Home',
+        link: '/work-with-me'
+      },
+    ]
+  })
+}
+</script>
+<style lang="scss" scoped>
+$menuFontSize: 24px;
+header {
+  width: 100%;
+  position: fixed;
+  top:0;
+  max-width: 980px;
+  background: #ffffff;
+  .item-link.v-tab.v-tab{
+    color: $lightenGray;
+    font-weight: 700;
+    font-size: $menuFontSize;
+    border-top-right-radius: $radius;
+    border-top-left-radius: $radius;
+  }
+  .item-link.v-tab.v-tab.v-tab--active{
+    background: $lightenGray;
+    color: $white;
+    border: 1px solid $black;
+    border-bottom: 0;
+  }
+}
+.theme--light.v-tabs > .v-tabs-bar .v-tab:not(.v-tab--active), .theme--light.v-tabs > .v-tabs-bar .v-tab:not(.v-tab--active) > .v-icon, .theme--light.v-tabs > .v-tabs-bar .v-tab:not(.v-tab--active) > .v-btn, .theme--light.v-tabs > .v-tabs-bar .v-tab--disabled{
+  color: $lightenGray;
+}
+</style>
