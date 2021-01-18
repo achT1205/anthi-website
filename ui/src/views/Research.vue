e3fe15829faf52cf02658c37579629cc4cec7477
achT1205 <achilletuglo12@gmail.com>
2021-01-18 11:05:38 +0100

    commit

--- ui/src/views/Research.vue
+++ ui/src/views/Research.vue
@@ -3,52 +3,12 @@
     <v-card elevation="0">
       <h2 class="custom-title">Main Research Interests</h2>
       <div class="d-flex flex-no-wrap justify-start">
-        <!--
+
         <v-row>
           <v-col cols="12">
             <img
             class="triangle_new_withText"
               src="@/assets/images/triangle_new_withText-01.png"
-              alt="pyramide research interests"
-            />
-          </v-col>
-        </v-row>
-       -->
-
-        <v-row>
-          <v-col cols="12" sm="12" lg="6">
-            <v-sheet class="ml-5 mt-4">
-              <div>
-                How to achieve a
-                <strong class="link-1">balanced human-machine symbiosis</strong>
-                with human - centered AI resulting in mixed - initiative,
-                transparent and understandable decision support tools
-              </div>
-            </v-sheet>
-            <v-sheet class="ml-5 mt-5">
-              <div>
-                How to design tools that
-                <div>
-                  - <strong class="link-1">represent</strong> information
-                  effectively to the user and <br />- provide
-                  <strong class="link-1">interaction</strong> means can allow
-                  information to be constructed, tested, refined and shared
-                </div>
-              </div>
-            </v-sheet>
-            <v-sheet class="ml-5 mt-5">
-              <div>
-                How people <strong class="link-1">should</strong> ,
-                <strong class="link-1">could</strong> and
-                <strong class="link-1">do make decisions</strong>
-              </div>
-            </v-sheet>
-          </v-col>
-          <v-col cols="12" sm="12" lg="6">
-            <img
-              class="img-fluid"
-              height="260"
-              src="@/assets/images/triangle_new-01.png"
               alt="pyramide research interests"
             />
           </v-col>
